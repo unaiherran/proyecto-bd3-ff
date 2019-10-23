1ccd6e711a46c1cbb13525082eb956b7fdd2a1a0
Unai Herran <unai.herran@gmail.com>
2019-10-23 22:55:33 +0200

    pruebas de acceso a la base de datos

---
+++ Cluster/poner_cluster_en_db.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from datetime import datetime, date, timedelta
+
+import mysql.connector
+from secret import *
+
+connection = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    passwd=db_passwd,
+    database=db_database,
+    port=db_port
+)
+
+if connection.is_connected():
+    cur = connection.cursor()
+    q = "SELECT * FROM AuxCamaras;"
+    cur.execute(q)
+
+    data = cur.fetchall()
+    print(data)
+
