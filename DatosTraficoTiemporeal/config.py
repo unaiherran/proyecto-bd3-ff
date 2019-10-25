00b6055659a96da1e0829b5735971f0a4bea8638
ivan <ivan@myhappyhour.me>
2019-10-25 20:00:57 +0200

    La configuracion

---
+++ DatosTraficoTiemporeal/config.py
@@ -0,0 +1,7 @@
+# db_host="database-1.cluster-csy1i72nclox.eu-west-1.rds.amazonaws.com",
+db_host = "localhost"
+db_user = "admin"
+db_passwd = ""
+db_database = 'proyecto'
+# db_port="3306"
+db_port = "33061"
