8658b15afa4e5aecc31cb92b8a677ab9f1bfbede
Unai Herran <unai.herran@gmail.com>
2019-10-29 20:44:08 +0100

    clusterizar camaras

--- Cluster/poner_cluster_en_db.py
+++ Cluster/poner_cluster_en_db.py
@@ -15,42 +15,80 @@ connection = mysql.connector.connect(
 
 modelo = load_model("camaras_cluster.joblib")
 
-lista = modelo.cluster_centers_.tolist()
 
-if connection.is_connected():
-    cur = connection.cursor()
-i = 0
-for l in lista:
-    longitud = l[0]
-    latitud = l[1]
 
-    sql = f'INSERT INTO Cluster (id_cluster,longitud, latitud) values ' \
-                      f'({i}, {longitud}, {latitud});'
+def poblar_cluster():
+    lista = modelo.cluster_centers_.tolist()
 
-    cur.execute(sql)
+    if connection.is_connected():
+        cur = connection.cursor()
+    i = 0
+    for l in lista:
+        longitud = l[0]
+        latitud = l[1]
 
-    connection.commit()
-    i += 1
+        sql = f'INSERT INTO Cluster (id_cluster,longitud, latitud) values ' \
+                          f'({i}, {longitud}, {latitud});'
 
-#if connection.is_connected():
-    #cur = connection.cursor()
-    # q = "SELECT * FROM SensoresTrafico;"
-    # cur.execute(q)
-    #
-    # data = cur.fetchall()
-    #
-    # for d in data:
-    #     print (d)
-    #     id = d[0]
-    #     longitud = d[5]
-    #     latitud = d[6]
-    #
-    #     cluster = coordenadas_a_cluster(longitud, latitud, modelo)
-    #
-    #     sql = f'UPDATE SensoresTrafico SET cluster = {cluster} WHERE id = {id};'
-    #
-    #     cur.execute(sql)
-    #
-    #     connection.commit()
-    #
-    #     print(id, cluster, cur.rowcount, "records affected")
+        cur.execute(sql)
+
+        connection.commit()
+        i += 1
+
+
+def clusterizar_sensores():
+    if connection.is_connected():
+        cur = connection.cursor()
+        q = "SELECT * FROM SensoresTrafico;"
+        cur.execute(q)
+
+        data = cur.fetchall()
+
+        for d in data:
+            print (d)
+            id = d[0]
+            longitud = d[5]
+            latitud = d[6]
+
+            cluster = coordenadas_a_cluster(longitud, latitud, modelo)
+
+            sql = f'UPDATE SensoresTrafico SET cluster = {cluster} WHERE id = {id};'
+
+            cur.execute(sql)
+
+            connection.commit()
+
+            print(id, cluster, cur.rowcount, "records affected")
+
+
+def clusterizar_camaras():
+    if connection.is_connected():
+        cur = connection.cursor()
+        q = "SELECT * FROM CamarasTrafico;"
+        cur.execute(q)
+
+        data = cur.fetchall()
+
+        for d in data:
+            print(d)
+            id = d[0]
+            longitud = d[5]
+            latitud = d[6]
+
+            cluster = coordenadas_a_cluster(longitud, latitud, modelo)
+
+            sql = f'UPDATE CamarasTrafico SET cluster = {cluster} WHERE id = {id};'
+
+            cur.execute(sql)
+
+            connection.commit()
+
+            print(id, cluster, cur.rowcount, "records affected")
+
+
+def main():
+    clusterizar_camaras()
+
+
+if __name__ == '__main__':
+    main()
