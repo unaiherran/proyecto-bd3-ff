f2f68b1a23783ed02adca564b210d1085029a332
Sergio García Charameli <sgcharameli@gmail.com>
2019-11-25 21:22:14 +0100

    Se actualiza ddl y script de carga eventos con estadísticas del proceso

--- DatosEventos/ddl.sql
+++ DatosEventos/ddl.sql
@@ -10,3 +10,10 @@ CREATE TABLE DatosEventos (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+-- INSERT INTO DatosEventos(fecha, gratuito, titulo, longitud, latitud, cluster) VALUES ('2019-10-25 11:00:00', 0, "Circuit des Yeux's", -3.651841641389484, 40.42342659084215, 7);
+
+ALTER TABLE DatosEventos ADD CONSTRAINT fk_eve_clu FOREIGN KEY (cluster) REFERENCES Cluster(id_cluster);
+ALTER TABLE DatosEventos ADD INDEX fk_eve_clu_idx (cluster ASC);
+
+CREATE UNIQUE INDEX eve_unique_idx ON DatosEventos(fecha, cluster);
