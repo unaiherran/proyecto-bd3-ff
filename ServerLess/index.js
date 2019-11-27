b9e83329a5bbee653da3285b3fb6db3649ccd22e
Felipe Martínez <fmaralv@gmail.com>
2019-11-27 21:13:07 +0100

    Parametrizacion de Lamdba y DatePicker en web

--- ServerLess/index.js
+++ ServerLess/index.js
@@ -2,7 +2,6 @@
  * Created by fmaralv on 10/11/19.
  */
 
-
 var mysql = require('mysql');
 var config = require('./config.json');
 
@@ -13,14 +12,31 @@ var pool  = mysql.createPool({
     database : config.dbname
 });
 
-pool.getConnection(function(err, connection) {
-    // Use the connection
-    connection.query('SELECT * from Cluster', function (error, results, fields) {
-        // And done with the connection.
-        connection.release();
-        // Handle error after the release.
-        if (error) throw error;
-        else console.log(results[0].densidad_trafico);
-        process.exit();
+
+exports.handler=  (event, context, callback) => {
+    //prevent timeout from waiting event loop
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    console.info("EVENT\n" + JSON.stringify(event, null, 2))
+
+
+
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        connection.query('SELECT Cluster.latitud, Cluster.longitud, predict.ocu_real, predict.ocu_pred  FROM predict INNER JOIN Cluster ON predict.cluster = Cluster.id_cluster WHERE predict.fecha BETWEEN str_to_date(\''+ event['fecha_ini'] + '\', \'%d/%m/%Y %H:%i\') AND str_to_date(\'' + event['fecha_fin'] + '\', \'%d/%m/%Y %H:%i\')', function (error, results, fields) {
+
+            //
+            // And done with the connection.
+            connection.release();
+            connection.destroy();
+
+
+            // Handle error after the release.
+            if (error) {
+                callback(error);
+            }else{
+                callback(null,results);
+            }
+        });
     });
-});
+};
